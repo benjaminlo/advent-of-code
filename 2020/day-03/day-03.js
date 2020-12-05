bb7c9e6adb02bae3f20716272e1a061ce4841dce
Benjamin Lo <benjamin.lo246@gmail.com>
2020-12-05 01:23:58 -0500

    Add solution for 2020 Day 3 Part 2

--- 2020/day-03/day-03.js
+++ 2020/day-03/day-03.js
@@ -5,15 +5,26 @@ const input = fs
   .trim()
   .split("\n");
 
-const getNumTrees = (input) => {
+const getNumTrees = (input, right, down) => {
   let numTrees = 0;
   let xPos = 0;
-  input.forEach((row) => {
-    if (row[xPos % row.length] === "#") numTrees++;
-    xPos += 3;
-  });
+  for (let i = 0; i < input.length; i += down) {
+    if (input[i][xPos % input[i].length] === "#") numTrees++;
+    xPos += right;
+  }
 
   return numTrees;
 };
 
-console.log(getNumTrees(input));
+const multiplyNumTreesForSlopes = (input) => {
+  return (
+    getNumTrees(input, 1, 1) *
+    getNumTrees(input, 3, 1) *
+    getNumTrees(input, 5, 1) *
+    getNumTrees(input, 7, 1) *
+    getNumTrees(input, 1, 2)
+  );
+};
+
+console.log(getNumTrees(input, 3, 1));
+console.log(multiplyNumTreesForSlopes(input));
